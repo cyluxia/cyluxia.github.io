f41a612758f2a846f693d6e4fcb54d2dd618bec9
cyluxia <69870772+cyluxia@users.noreply.github.com>
2020-08-28 14:53:12 +0200

    Update CyluxiA-Pingr.bat

--- CyluxiA-Pingr.bat
+++ CyluxiA-Pingr.bat
@@ -1,34 +1,37 @@
 @echo off
 color 3
 title Pinger
-    
-echo   .g8"""bg               MM                           db        db      
-echo .dP'     `M              MM                                    ;MM:     
-echo dM'       ` `7M'   `MF'  MM    MM    MM   7M     MF  7MM      ,V^MM.    
-echo MM            VA   ,V    MM    MM    MM    `VA ,V'    MM     ,M  `MM    
-echo MM.            VA ,V     MM    MM    MM      XMX      MM     AbmmmqMA   
-echo `Mb.     ,'     VVV      MM    MM    MM    ,V' VA.    MM    A'     VML  
-echo   `"bmmmd'      ,V       MML.  `Mbod"YM   AM     MA   MML  AM       MM
-echo                ,V                                                       
-echo              OOb"                                                        
 
-
-
+echo -
+echo - 
+echo 	 @@@@@@@  @@@ @@@  @@@       @@@  @@@  @@@  @@@  @@@   @@@@@@   
+echo 	 @@@@@@@@  @@@ @@@  @@@       @@@  @@@  @@@  @@@  @@@  @@@@@@@@  
+echo 	 !@@       @@! !@@  @@!       @@!  @@@  @@!  !@@  @@!  @@!  @@@  
+echo 	 !@!       !@! @!!  !@!       !@!  @!@  !@!  @!!  !@!  !@!  @!@  
+echo 	 !@!        !@!@!   @!!       @!@  !@!   !@@!@!   !!@  @!@!@!@!  
+echo 	 !!!         @!!!   !!!       !@!  !!!    @!!!    !!!  !!!@!!!!  
+echo 	 :!!         !!:    !!:       !!:  !!!   !: :!!   !!:  !!:  !!!  
+echo 	 :!:         :!:     :!:      :!:  !:!  :!:  !:!  :!:  :!:  !:!   
+echo 	 ::: :::     ::     :: ::::  ::::: ::   ::  :::   ::  ::   :::  
+echo 	 :: :: :     :     : :: : :   : :  :    :   ::   :     :   : :   
+echo -
 echo                  ________ _____                          
 echo                  ___  __ \___(_)_______ _______ _________
 echo                  __  /_/ /__  / __  __ \__  __ `/__  ___/
 echo                  _  ____/ _  /  _  / / /_  /_/ / _  /    
 echo                  /_/      /_/   /_/ /_/ _\__, /  /_/     
-echo                                         /____/           
+echo                                         /____/          
+echo -
 echo       ==============================================================
 echo             =              Control+C to Stop                =
 echo       ==============================================================
+echo -
 
 set /p IP=Enter IP:
 :top
 PING -n 1 %Ip% | FIND "TTL="
 title :: Pinging: %Ip% ::
-IF ERRORLEVEL 1 (echo IP downed)
+IF ERRORLEVEL 1 (echo IP Pinged)
 set /a num=(%Random%%%9)+1
 color %num%
 ping -t 1 0 10 127.0.0.1 >nul
